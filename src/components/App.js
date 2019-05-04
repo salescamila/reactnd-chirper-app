a133805939823de3b8d724f29db6bc4990668e42
Camila <salescamila1@gmail.com>
2019-05-04 13:55:53 -0400

    commit inicial

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
+
+class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData())
+  }
+  render() {
+    return (
+      <div>
+        Starter Code
+      </div>
+    )
+  }
+}
+
+export default connect()(App)
