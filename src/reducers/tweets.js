421ac49396d7d6c19ea570c69ddde7ec3ceb3673
salescamila <salescamila1@gmail.com>
2019-05-06 16:57:12 -0400

    Like Tweet Reducer

--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -7,6 +7,16 @@ export default function tweets (state = {}, action) {
         ...state,
         ...action.tweets
       }
+    case TOGGLE_TWEET :
+      return {
+        ...state,
+        [action.id]: {
+          ...state[action.id],
+          likes: action.hasLiked === true
+            ? state[action.id].likes.filter((uid) => uid !== action.authedUser)
+            : state[action].likes.concat([action.authedUser])
+        }
+      }
     default :
       return state
   }
