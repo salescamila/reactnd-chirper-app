3451c48beb1df00b5c38050d5690cb7fa12451ad
Camila <salescamila1@gmail.com>
2019-05-06 00:10:41 -0400

    Handling a Parent Tweet

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -4,7 +4,14 @@ import { formatTweet } from '../utils/helpers'
 
 class Tweet extends Component {
   render () {
+    const { tweet } = this.props
+
+    if (tweet === null) {
+      return <p>This tweet doesn't exists</p>
+    }
+
     console.log(this.props)
+
     return (
       <div className='tweet'>
 
@@ -15,10 +22,13 @@ class Tweet extends Component {
 
 function mapStateToProps({authedUser, users, tweets}, { id }){
   const tweet = tweets[id]
+  const parentTweet = tweet ? tweets[tweet.replyingTo] : null
 
   return {
     authedUser,
-    tweet: formatTweet(tweet, users[tweet.author], authedUser)
+    tweet: tweet
+      ? formatTweet(tweet, users[tweet.author], authedUser, parentTweet)
+      : null
   }
 }
 
