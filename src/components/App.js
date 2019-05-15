bad3b35980c2d53e5aa40a0140fd1c8ab0ce97a6
Camila <salescamila1@gmail.com>
2019-05-14 23:18:29 -0400

    Tweet Page

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { handleInitialData } from '../actions/shared'
 import Dashboard from './Dashboard'
-import { LoadingBar } from 'react-redux-loading'
+import LoadingBar from 'react-redux-loading'
 import NewTweet from './NewTweet'
+import TweetPage from './TweetPage'
 
 class App extends Component {
   componentDidMount() {
@@ -15,10 +16,16 @@ class App extends Component {
         <LoadingBar />
         {this.props.loading === true
           ? null
-          : <NewTweet />}
+          : <TweetPage match={{params: {id: '8xf0y6ziyjabvozdd253nd'}}}/>}
       </div>
     )
   }
 }
 
-export default connect()(App)
+function mapStateToProps ({ authedUser }) {
+  return {
+    loading: authedUser === null
+  }
+}
+
+export default connect(mapStateToProps)(App)
